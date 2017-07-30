0eecd3b322ec3449a36bfb6ebfccae6b1a9ebc76
Hieu <lamtronghieu96@gmail.com>
2017-07-30 20:48:50 +0200

    no message

---
+++ Tower of Hanoi/Tower of Hanoi/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Tower_of_Hanoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Timelapse = new System.Windows.Forms.Label();
+            this.Steps = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Timelapse
+            // 
+            this.Timelapse.AutoSize = true;
+            this.Timelapse.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Timelapse.Location = new System.Drawing.Point(292, 43);
+            this.Timelapse.Name = "Timelapse";
+            this.Timelapse.Size = new System.Drawing.Size(141, 25);
+            this.Timelapse.TabIndex = 35;
+            this.Timelapse.Text = "Time spent: 0s";
+            // 
+            // Steps
+            // 
+            this.Steps.AutoSize = true;
+            this.Steps.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Steps.Location = new System.Drawing.Point(292, 3);
+            this.Steps.Name = "Steps";
+            this.Steps.Size = new System.Drawing.Size(149, 40);
+            this.Steps.TabIndex = 34;
+            this.Steps.Text = "Steps: 0";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(560, 352);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(111, 52);
+            this.button7.TabIndex = 33;
+            this.button7.Text = "Load";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(392, 352);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(111, 52);
+            this.button6.TabIndex = 32;
+            this.button6.Text = "Save Game";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(242, 352);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 52);
+            this.button5.TabIndex = 31;
+            this.button5.Text = "Auto Play";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(67, 352);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 52);
+            this.button4.TabIndex = 30;
+            this.button4.Text = "New Game";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(500, 303);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 36);
+            this.button3.TabIndex = 29;
+            this.button3.Text = "Select";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(329, 303);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 36);
+            this.button2.TabIndex = 28;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(155, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox9
+            // 
+            this.textBox9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.textBox9.Location = new System.Drawing.Point(162, 178);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(60, 20);
+            this.textBox9.TabIndex = 26;
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.textBox8.Location = new System.Drawing.Point(148, 198);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(88, 20);
+            this.textBox8.TabIndex = 25;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.Color.Yellow;
+            this.textBox7.Location = new System.Drawing.Point(138, 218);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(108, 20);
+            this.textBox7.TabIndex = 24;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.textBox6.Location = new System.Drawing.Point(128, 238);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(128, 20);
+            this.textBox6.TabIndex = 23;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.Red;
+            this.textBox5.Location = new System.Drawing.Point(113, 258);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(158, 20);
+            this.textBox5.TabIndex = 22;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Gray;
+            this.textBox4.Location = new System.Drawing.Point(525, 100);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(22, 179);
+            this.textBox4.TabIndex = 21;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Gray;
+            this.textBox3.Location = new System.Drawing.Point(355, 100);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(22, 179);
+            this.textBox3.TabIndex = 20;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Gray;
+            this.textBox2.Location = new System.Drawing.Point(182, 100);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(22, 179);
+            this.textBox2.TabIndex = 19;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Gray;
+            this.textBox1.Location = new System.Drawing.Point(87, 277);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(559, 20);
+            this.textBox1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 431);
+            this.Controls.Add(this.Timelapse);
+            this.Controls.Add(this.Steps);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Timelapse;
+        private System.Windows.Forms.Label Steps;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Tower of Hanoi/Tower of Hanoi/Form1.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Tower_of_Hanoi
+{
+    public partial class Form1 : Form
+    {
+        #region Variable Declaration
+        public bool[,] tower = new bool[3, 5] {{ true, true, true, true, true },
+        { false, false, false, false, false },{ false, false, false, false, false } };
+        Timer t, t1;
+        TextBox ring, topRing;
+        bool ringIsUp = false;
+        int mid;
+        int count = 0;
+        int time = 0;
+        int step = 0;
+        int oldX = 0;
+        int oldY = 0;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+            Timer();
+        }
+        public void RingUp(int num)
+        {
+            int y = 178;
+            for (int i = 0; i < 5; i++)
+            {
+                if (tower[num, i] == true)
+                {
+                    GetRing(num, y, ref ring);
+                    oldX = ring.Location.X;
+                    oldY = ring.Location.Y;
+                    ring.Location = new Point(mid - (ring.Width / 2), 70);
+                    tower[num, i] = false;
+                    break;
+                }
+                y += 20;
+            }
+        }
+        public void RingDown(int num)
+        {
+            int y = 258;
+            for (int i = 4; i >= 0; i--)
+            {
+                if (tower[num, i] == false)
+                {
+                    SetRing(num, y);
+                    tower[num, i] = true;
+                    break;
+                }
+                y -= 20;
+            }
+        }
+        public void GetRing(int num, int y, ref TextBox rings)
+        {
+            if (num == 0)
+                mid = 192;
+            else if (num == 1)
+                mid = 365;
+            else if (num == 2)
+                mid = 535;
+            if ((textBox9.Location.X + textBox9.Width / 2) == mid && textBox9.Location.Y == y)
+                rings = textBox9;
+            else if ((textBox8.Location.X + textBox8.Width / 2) == mid && textBox8.Location.Y == y)
+                rings = textBox8;
+            else if ((textBox7.Location.X + textBox7.Width / 2) == mid && textBox7.Location.Y == y)
+                rings = textBox7;
+            else if ((textBox6.Location.X + textBox6.Width / 2) == mid && textBox6.Location.Y == y)
+                rings = textBox6;
+            else if ((textBox5.Location.X + textBox5.Width / 2) == mid && textBox5.Location.Y == y)
+                rings = textBox5;
+        }
+        public void GetTopRing(int num)
+        {
+            int y = 178;
+            for (int i = 0; i < 5; i++)
+            {
+                if (tower[num, i] == true)
+                {
+                    GetRing(num, y, ref topRing);
+                    break;
+                }
+                y += 20;
+            }
+        }
+        public void SetRing(int mid, int y)
+        {
+            if (mid == 0)
+                ring.Location = new Point(192 - (ring.Width / 2), y);
+            else if (mid == 1)
+                ring.Location = new Point(365 - (ring.Width / 2), y);
+            else if (mid == 2)
+                ring.Location = new Point(535 - (ring.Width / 2), y);
+        }
+        public void RingMove(int num)
+        {
+            if (tower[num, 4] == false)
+            {
+                if (ringIsUp == true)
+                {
+                    RingDown(num);
+                    ringIsUp = false;
+                    if (!(ring.Location.X == oldX && ring.Location.Y == oldY))
+                    {
+                        step++;
+                        Steps.Text = "Steps: " + step;
+                    }
+                }
+            }
+            else
+            {
+                if (ringIsUp == true)
+                {
+                    GetTopRing(num);
+                    if (ring.Width < topRing.Width)
+                    {
+                        RingDown(num);
+                        ringIsUp = false;
+                        if (!(ring.Location.X == oldX && ring.Location.Y == oldY))
+                        {
+                            step++;
+                            Steps.Text = "Steps: " + step;
+                        }
+                    }
+                }
+                else
+                {
+                    RingUp(num);
+                    ringIsUp = true;
+                }
+            }
+        }
+        public void LegalMoveBetween(int num1, int num2)
+        {
+            RingMove(num1);
+            if (ringIsUp)
+            {
+                RingMove(num2);
+                if (ringIsUp)
+                {
+                    RingMove(num1);
+                    RingMove(num2);
+                    RingMove(num1);
+                }
+            }
+            else
+            {
+                RingMove(num2);
+                RingMove(num1);
+            }
+        }
+        public void Auto()
+        {
+            Clear();
+
+            t = new Timer();
+            t.Interval = 1000;
+            t.Tick += new EventHandler(this.t_Tick);
+            t.Start();
+        }
+        public void Clear()
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                tower[0, j] = true;
+                tower[1, j] = false;
+                tower[2, j] = false;
+            }
+            textBox9.Location = new Point(162, 178);
+            textBox8.Location = new Point(148, 198);
+            textBox7.Location = new Point(138, 218);
+            textBox6.Location = new Point(128, 238);
+            textBox5.Location = new Point(113, 258);
+            time = 0;
+            step = 0;
+            Timelapse.Text = "Time spent: " + time + "s";
+            Steps.Text = "Steps: " + step;
+        }
+        public void Timer()
+        {
+            t1 = new Timer();
+            t1.Interval = 1000;
+            t1.Tick += new EventHandler(this.t1_Tick);
+            t1.Start();
+        }
+        public void Save()
+        {
+
+            string tower1 = String.Format("{0};{1};{2};{3};{4}", tower[0, 0], tower[0, 1], tower[0, 2], tower[0, 3], tower[0, 4]);
+            string tower2 = String.Format("{0};{1};{2};{3};{4}", tower[1, 0], tower[1, 1], tower[1, 2], tower[1, 3], tower[1, 4]);
+            string tower3 = String.Format("{0};{1};{2};{3};{4}", tower[2, 0], tower[2, 1], tower[2, 2], tower[2, 3], tower[2, 4]);
+            string values = String.Format("{0};{1};{2};{3}", oldX, oldY, time, step);
+            string ringsposition = String.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9}", textBox9.Location.X, textBox9.Location.Y, textBox8.Location.X, textBox8.Location.Y, textBox7.Location.X, textBox7.Location.Y, textBox6.Location.X, textBox6.Location.Y, textBox5.Location.X, textBox5.Location.Y);
+            string rings = String.Format("{0};{1}", ToStringRing(ref ring), ToStringRing(ref topRing));
+            string file = ToStringDateTime();
+            using (StreamWriter sw = new StreamWriter(file, true))
+            {
+                sw.WriteLine(tower1);
+                sw.WriteLine(tower2);
+                sw.WriteLine(tower3);
+                sw.WriteLine(ringIsUp);
+                sw.WriteLine(ringsposition);
+                sw.WriteLine(values);
+                sw.WriteLine(rings);
+            }
+        }
+        public void LoadGame()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                {
+                    string[] tower1 = sr.ReadLine().ToLower().Split(';');
+                    string[] tower2 = sr.ReadLine().ToLower().Split(';');
+                    string[] tower3 = sr.ReadLine().ToLower().Split(';');
+                    bool ringisup = Convert.ToBoolean(sr.ReadLine());
+                    string[] ringposition = sr.ReadLine().Split(';');
+                    string[] values = sr.ReadLine().Split(';');
+                    string[] rings = sr.ReadLine().Split(';');
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        tower[0, i] = Convert.ToBoolean(tower1[i]);
+                        tower[1, i] = Convert.ToBoolean(tower2[i]);
+                        tower[2, i] = Convert.ToBoolean(tower3[i]);
+                    }
+                    ringIsUp = ringisup;
+                    textBox9.Location = new Point(Convert.ToInt16(ringposition[0]), Convert.ToInt16(ringposition[1]));
+                    textBox8.Location = new Point(Convert.ToInt16(ringposition[2]), Convert.ToInt16(ringposition[3]));
+                    textBox7.Location = new Point(Convert.ToInt16(ringposition[4]), Convert.ToInt16(ringposition[5]));
+                    textBox6.Location = new Point(Convert.ToInt16(ringposition[6]), Convert.ToInt16(ringposition[7]));
+                    textBox5.Location = new Point(Convert.ToInt16(ringposition[8]), Convert.ToInt16(ringposition[9]));
+                    oldX = Convert.ToInt16(values[0]);
+                    oldY = Convert.ToInt16(values[1]);
+                    time = Convert.ToInt16(values[2]);
+                    step = Convert.ToInt16(values[3]);
+                    Steps.Text = "Steps: " + step;
+                    BackToBox(rings[0], ref ring);
+                    BackToBox(rings[1], ref topRing);
+                }
+            }
+        }
+        public string ToStringDateTime()
+        {
+            DateTime now = DateTime.Now;
+            string date = now.Day.ToString();
+            string month = now.Month.ToString();
+            string year = now.Year.ToString();
+
+            string time = now.Hour.ToString();
+            string minute = now.Minute.ToString();
+            string second = now.Second.ToString();
+
+            string str = String.Format("{0}-{1}-{2} {3}.{4}.{5}.txt", date, month, year, time, minute, second);
+
+            return str;
+        }
+        public string ToStringRing(ref TextBox rings)
+        {
+            if (rings == textBox9)
+            {
+                string str = "textBox9";
+                return str;
+            }
+            else if (rings == textBox8)
+            {
+                string str = "textBox8";
+                return str;
+            }
+            else if (rings == textBox7)
+            {
+                string str = "textBox7";
+                return str;
+            }
+            else if (rings == textBox6)
+            {
+                string str = "textBox6";
+                return str;
+            }
+            else if (rings == textBox5)
+            {
+                string str = "textBox5";
+                return str;
+            }
+            else return "";
+
+        }
+        public TextBox BackToBox(string ringtext, ref TextBox rings)
+        {
+            if (ringtext == "textBox9")
+            {
+                rings = textBox9;
+                return rings;
+            }
+            else if (ringtext == "textBox8")
+            {
+                rings = textBox8;
+                return rings;
+            }
+            else if (ringtext == "textBox7")
+            {
+                rings = textBox7;
+                return rings;
+            }
+            else if (ringtext == "textBox6")
+            {
+                rings = textBox6;
+                return rings;
+            }
+            else if (ringtext == "textBox5")
+            {
+                rings = textBox5;
+                return rings;
+            }
+            else return null;
+        }
+        public void t1_Tick(object sender, EventArgs e)
+        {
+            time++;
+            Timelapse.Text = "Time spent: " + time + "s";
+            if ((textBox9.Location.X == 505 || textBox9.Location.X == 335) && textBox9.Location.Y == 178)
+            {
+                t1.Stop();
+                t1.Dispose();
+                MessageBox.Show("You Win!");
+            }
+        }
+        public void t_Tick(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                LegalMoveBetween(0, 2);
+                count++;
+                if (textBox9.Location.X == 505 && textBox9.Location.Y == 178)
+                {
+                    t.Stop();
+                    t.Dispose();
+                }
+            }
+            else if (count == 1)
+            {
+                LegalMoveBetween(0, 1);
+                count++;
+            }
+            else if (count == 2)
+            {
+                LegalMoveBetween(2, 1);
+                count = 0;
+            }
+        }
+        public void button1_Click(object sender, EventArgs e)
+        {
+            RingMove(0);
+
+        }
+        public void button2_Click(object sender, EventArgs e)
+        {
+            RingMove(1);
+        }
+        public void button3_Click(object sender, EventArgs e)
+        {
+            RingMove(2);
+        }
+        public void button4_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+        public void button5_Click(object sender, EventArgs e)
+        {
+            Auto();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            LoadGame();
+        }
+
+    }
+}
